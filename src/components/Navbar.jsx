8a40a1981441793eb1cb2ffabd67b521c2e4ca06
Areeb khan <areebkhan4575@gmail.com>
2025-07-22 19:59:42 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({settoken}) => {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
       <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
-      <button onClick={()=>settoken("")} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout </button>
+      <button onClick={()=>{settoken("");localStorage.removeItem("token");}} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout </button>
     </div>
   )
 }
